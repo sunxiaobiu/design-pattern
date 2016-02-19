597b4461d366a836ad1be4467a2a868d5f5421cc
xiaoyu.sun.sh <sunxiaobiu@gmail.com>
2016-02-19 17:25:05 +0800

    composite

---
+++ src/Composite/Tree.java
@@ -0,0 +1,24 @@
+package Composite;
+
+/**
+ * Created by sun on 16/2/19.
+ */
+public class Tree {
+
+    TreeNode root = null;
+
+    public Tree(String name){
+        root = new TreeNode(name);
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree("A");
+        TreeNode nodeB = new TreeNode("B");
+        TreeNode nodeC = new TreeNode("C");
+
+        nodeB.add(nodeC);
+        tree.root.add(nodeB);
+        System.out.println("build the tree finished!");
+    }
+
+}
