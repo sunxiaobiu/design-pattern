8b285f8d0bd2c8fb4b3f59d61334a0d565e24227
xiaoyu.sun.sh <sunxiaobiu@gmail.com>
2016-02-23 14:25:18 +0800

    flyweight

--- src/templateMethodPattern/bootstrap.java
+++ src/templateMethodPattern/bootstrap.java
@@ -10,4 +10,27 @@ public class bootstrap {
         int result = cal.calculate(exp, "\\+");
         System.out.println(result);
     }
+//    private static int max = 1000000000;
+//
+//public static void main(String[] args) {
+//    double num = 0.0;
+//    for(int i=0; i<=max; i++){
+//        num += calculate();
+//    }
+//    System.out.println(num/max);
+//}
+//
+//    private static int calculate(){
+//        double result = 0;
+//        int num = 0;
+//        while(result <= 1.0){
+//            double random = Math.random();
+//            while(random == 0.0){
+//                random = Math.random();
+//            }
+//            result += random;
+//            num ++;
+//        }
+//        return num;
+//    }
 }
